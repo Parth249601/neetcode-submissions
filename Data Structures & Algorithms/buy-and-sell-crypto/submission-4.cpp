ee9efe62e890fd7171c44d38ee60cf2fcf44af85
Parth Bansal <bansalparth48@gmail.com>
2026-08-11 19:59:16 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int helper(vector<int>& prices, int left, int right, vector<vector<int>>& dp){
+        int n = prices.size();
+        if(left >= n) return 0;
+        if(left == right) return dp[left][right] = 0;
+
+        if(dp[left][right] != -1) return dp[left][right];
+
+        if(right - left == 1){
+            return dp[left][right] = prices[right] - prices[left] >= 0 ? prices[right] - prices[left] : 0;
+        }
+
+        int num1 = helper(prices, left+1, right, dp);
+        int num2 = helper(prices, left, right- 1, dp);
+        int num3 = prices[right] - prices[left];
+        return dp[left][right] = max(num3, max(num1, num2));
+        
+
+    }
+    int maxProfit(vector<int>& prices) {
+        //if h(x) < h(y); x++ to see if we can locate even lower h(z) < h(x), but using similar logic i could do y-- 
+
+        int n= prices.size();
+        int best = 0;
+        int minLeft = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            best = max(best, prices[i] - minLeft);
+            minLeft = min(minLeft, prices[i]);
+        }
+        return best;
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // return helper(prices, 0, n-1, dp);
+
+    }
+};
